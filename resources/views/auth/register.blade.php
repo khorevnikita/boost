46566e3ee1d3f58e00b9ada05b29e71cd5484c75
NikKho <nh@codenrock.com>
2020-06-03 19:45:26 +0300

    mt

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
     <div class="container mb-3">
         <div class="row justify-content-center">
             <div class="col-md-10">
-                <div class="card">
+                <div class="card mt-3">
                     <div class="card-header">{{ __('Register') }}</div>
 
                     <div class="card-body">
