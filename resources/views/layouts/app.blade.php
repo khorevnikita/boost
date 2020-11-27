9dc8abe964a75d6960e3930f4c75f9d4f05d031a
NikKho <nh@codenrock.com>
2020-11-27 16:22:03 +0300

    new fb code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,11 +20,11 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '160623425763216');
+        fbq('init', '394692321943326');
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=160623425763216&ev=PageView&noscript=1"
+                   src="https://www.facebook.com/tr?id=394692321943326&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Facebook Pixel Code -->
     <meta charset="utf-8">
